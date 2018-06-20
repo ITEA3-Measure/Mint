38f1659955d09a1a37b13ff8c8a209a2ebd59552
ersilva <erika.f.silva.b@gmail.com>
2018-06-20 21:33:59 +0200

    delete redis test page

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var configurationRouter = require('./routes/configuration');
 var historyRouter = require('./routes/history');
 var alertMonitor = require('./routes/alertMonitor');
 var machines = require('./routes/machinesCron');
-var redisTest = require('./routes/redis');
 var subscriber = require('./examples/subscriber');
 var measurements = require('./examples/cronGetMeasurements');
 
@@ -37,7 +36,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/configure', configurationRouter);
 app.use('/history', historyRouter);
-app.use('/redis', redisTest);
 /*alertMonitor.registerTool.start();*/
 
 // catch 404 and forward to error handler
